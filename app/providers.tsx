e7ea1a5eca431bc5d62af3e381fdb75ac086b932
Ismael Loko <107557003+Lil-Code30@users.noreply.github.com>
2026-03-06 20:58:36 -0500

    fix: update Toaster position from bottom-left to bottom-right in Providers component

--- app/providers.tsx
+++ app/providers.tsx
@@ -15,7 +15,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
       <AuthProvider>
         <CartProvider>
           {children}
-          <Toaster position="bottom-left" />
+          <Toaster position="bottom-right" />
         </CartProvider>
       </AuthProvider>
     </QueryClientProvider>
